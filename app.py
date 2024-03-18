3b8a86120c149fe44f3444a098770e503733626c
Naufal Ramdhan <naufal.ramdhan@gmail.com>
2024-03-18 13:02:14 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+def luasSegitiga():
+    a = int(input("Masukkan sisi segitiga: "))
+    b = int(input("Masukkan tinggi segitiga: "))
+    luas = (a * b) / 2
+    print("Luas segitiga adalah: ", luas)
+
+
+luasSegitiga()
